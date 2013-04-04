0ce20e813a19002bc9db026698d5307563f54ebf
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2013-04-04 14:29:33 +0200

    composer

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-
 define('APP_DIR', __DIR__);
 
-\jtl\Connector\Application::getInstance()->run();
+use \jtl\Connector\Application\Application as MainApplication;
+
+MainApplication::getInstance()->run();
 ?>
