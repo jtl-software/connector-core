b350fab6278860e918947fcda34c264ffd711751
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2013-04-04 16:10:10 +0200

    index

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-define('APP_DIR', __DIR__);
+/**
+ * @copyright 2010-2013 JTL-Software GmbH 
+ */
+
+require_once(__DIR__ . "/../vendor/autoload.php");
 
 use \jtl\Connector\Application\Application as MainApplication;
 
