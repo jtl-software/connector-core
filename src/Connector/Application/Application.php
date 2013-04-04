565583ee3f5882516fe3aab486280fd643d159dd
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2013-04-04 13:05:38 +0200

    xxx

---
+++ src/Connector/Application/Application.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright 2010-2013 JTL-Software GmbH
+ * @package jtl\Connector\Application 
+ */
+
+namespace jtl\Connector\Application;
+
+use \jtl\Core\Application\Application as CoreApplication;
+
+/**
+ * Application Class
+ * 
+ * @access public
+ * @author Daniel Böhmer <daniel.boehmer@jtl-software.de>
+ */
+class Application extends CoreApplication
+{
+    /**
+     * (non-PHPdoc)
+     * @see \jtl\Core\Application\Application::run()
+     */
+	public function run()
+	{
+		
+	}
+}
+?>
