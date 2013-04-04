b9eeb0a916b2e0fc4c1faf9c149bd0f7dc54d67b
David Spickers <david.spickers@jtl-software.de>
2013-04-04 12:07:57 +0200

    phpunit eingefügt

---
+++ tests/Connector/Application/ApplicationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class ApplicationTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        //Dummy
+    }
+
+    public function testDefault() {
+        $this->assertTrue(true, 'Dude we\'re fucked true != true :-(');
+    }
+
+    public function testWriteTests() {
+        $this->assertTrue(false, 'You need to write Unit Test\'s for your Application!');
+    }
+
+}
