1df8d21bed6de70bb97e3bb7141cb4974cdcd198
David Spickers <david.spickers@jtl-software.de>
2013-04-16 16:11:49 +0200

    basis connector kann nun mit der config umgehen

--- src/jtl/Connector/Base/Connector.php
+++ src/jtl/Connector/Base/Connector.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright 2010-2013 JTL-Software GmbH
  * @package jtl\Connector\Base
@@ -8,6 +9,8 @@ namespace jtl\Connector\Base;
 
 use \jtl\Connector\Application\IEndpointConnector;
 use \jtl\Core\Utilities\Singleton;
+use \jtl\Core\Utilities\Config;
+use \jtl\Core\Exception\ConnectorException;
 
 /**
  * Base Connector
@@ -17,6 +20,33 @@ use \jtl\Core\Utilities\Singleton;
  */
 abstract class Connector extends Singleton implements IEndpointConnector
 {
-	
+
+  protected $config;
+
+  /**
+   * Setter connector config.
+   * 
+   * @param \jtl\Core\Utilities\Config $config
+   */
+  public function setConfig(Config $config)
+  {
+    $this->config = $config->load();
+  }
+
+  /**
+   * Returns the config.
+   * 
+   * @return object
+   * @throws ConnectorException
+   */
+  public function getConfig()
+  {
+    if (empty($this->config)) {
+      throw new ConnectorException('The connector configuration is not set!');
+    }
+    return $this->config;
+  }
+
 }
+
 ?>
