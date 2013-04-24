8be6bfe108fe8d42ed36c66fc8bce3231b31a893
David Spickers <david.spickers@jtl-software.de>
2013-04-24 11:21:52 +0200

    connector: features, init

---
+++ src/jtl/Connector/Controller/Connector.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ *
+ * @copyright 2010-2013 JTL-Software GmbH
+ * @package jtl\Connector\Application
+ */
+
+namespace jtl\Connector\Controller;
+
+use \jtl\Core\Controller\Controller as CoreController;
+use \jtl\Core\Exception\ControllerException;
+use \jtl\Connector\Result\Action;
+use \jtl\Core\Rpc\Error;
+
+/**
+ * Base Config Controller
+ *
+ * @access public
+ * @author David Spickers <david.spickers@jtl-software.de>
+ */
+class Connector extends CoreController
+{
+
+    /**
+     * Initialize the connector.
+     *
+     * @param mixed $params Can be empty or not defined and a string.
+     */
+    public function init($params = null)
+    {
+        $ret = new Action();
+        try {
+            $ret->setResult($this->getConfig()->read($params));
+            $ret->setHandled(true);
+        } catch (\Exception $e) {
+            $err = new Error();
+            $err->setCode($e->getCode());
+            $err->setMessage($e->getMessage());
+            $ret->setError($err);
+        }
+        return $ret;
+    }
+    /**
+     * Returns the connector features.
+     * 
+     * @param mixed $params Can be empty or not defined and a string.
+     */
+    public function features($params = null)
+    {
+        $ret = new Action();
+        try {
+            //@todo: irgend ne supertolle feature list methode
+            $features = array(); 
+            $ret->setResult($features);
+            $ret->setHandled(true);
+        } catch (\Exception $e) {
+            $err = new Error();
+            $err->setCode($e->getCode());
+            $err->setMessage($e->getMessage());
+            $ret->setError($err);
+        }
+        return $ret;
+    }
+
+}
