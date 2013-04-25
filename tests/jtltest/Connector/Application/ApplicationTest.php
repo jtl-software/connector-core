f0e5de7075b3ea7967cc880ace4932b288a61256
David Spickers <david.spickers@jtl-software.de>
2013-04-25 08:27:26 +0200

    unit tests angepasst

--- tests/jtltest/Connector/Application/ApplicationTest.php
+++ tests/jtltest/Connector/Application/ApplicationTest.php
@@ -8,10 +8,5 @@ class ApplicationTest extends PHPUnit_Framework_TestCase {
 
     public function testDefault() {
         $this->assertTrue(true, 'Dude we\'re fucked true != true :-(');
-    }
-
-    public function testWriteTests() {
-        $this->assertTrue(false, 'You need to write Unit Test\'s for your Application!');
-    }
 
 }
