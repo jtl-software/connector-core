0ac2902c93795b565ed00a0a7b1387eb97434d76
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2013-04-29 16:07:39 +0200

    Testing

--- src/jtl/Connector/Model/Product.php
+++ src/jtl/Connector/Model/Product.php
@@ -1,13 +1,22 @@
 <?php
 /**
  * @copyright 2010-2013 JTL-Software GmbH
- * @package jtl\Core\Model
+ * @package jtl\Connector\Model
  */
 
-namespace jtl\Core\Model;
+namespace jtl\Connector\Model;
+
+use \jtl\Core\Model\Model;
 
 abstract class Product extends Model
 {
+    protected $_id;
+    protected $_name;
     
+    abstract function setId($id);
+    abstract function getId();
+    abstract function setName($name);
+    abstract function getName();
+    abstract function convert(\stdClass $obj);
 }
 ?>
