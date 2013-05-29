a8975c8d8a1d7098ca2de3d2988bf61929597858
Christian Spoo <christian.spoo@jtl-software.com>
2013-05-29 08:52:08 +0200

    Move installer to a separate branch

---
+++ src/jtl/Connector/Installer/Step/FinishStep.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright 2010-2013 JTL-Software GmbH
+ * @package jtl\Connector\Installer
+ */
+namespace jtl\Connector\Installer\Step;
+
+/**
+ * Description of FinishStep
+ *
+ * @access public
+ * @author Christian Spoo <christian.spoo@jtl-software.de>
+ */
+class FinishStep extends InstallerStep
+{
+    protected $_template = 'step_finish';
+}
