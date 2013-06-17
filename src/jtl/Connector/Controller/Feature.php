3c9000d06a8c2278624a79623a352844149a8fa4
David Spickers <david.spickers@jtl-software.de>
2013-06-17 10:03:03 +0200

    1. Feature Controller eingebaut 2. Basis Producer, small fix 3. Standard Wawi Exporter eingebaut

--- src/jtl/Connector/Controller/Feature.php
+++ src/jtl/Connector/Controller/Feature.php
@@ -12,6 +12,10 @@ use \jtl\Core\Controller\Controller as CoreController;
 use \jtl\Core\Exception\ControllerException;
 use \jtl\Connector\Result\Action;
 use \jtl\Core\Rpc\Error;
+use jtl\Connector\Feature\Manager;
+use jtl\Connector\Feature\Producer;
+use jtl\Connector\Feature\Importer\Json as JsonImporter;
+use jtl\Connector\Feature\Exporter\Wawi as WawiExporter;
 
 /**
  * Base Features Controller
@@ -29,43 +33,58 @@ class Feature extends CoreController
      */
     public function pull($params = null)
     {
-//        $ret = new Action();
-//        try {
-//            $ret->setResult($this->getConfig()->reads($params));
-//            $ret->setHandled(true);
-//        } catch (\Exception $e) {
-//            if (isset($e->jtl)) {
-//                $ret->setHandled($e->jtl);
-//            }
-//            else {
-//                $ret->setHandled(false);
-//            }
-//            $err = new Error();
-//            $err->setCode($e->getCode());
-//            $err->setMessage($e->getMessage());
-//            $ret->setError($err);
-//        }
+        unset($params); //We don't need the params parameter
+        $man = new Manager(new Producer()); //Create our feature manager instance
+        $man->registerMethods(array('pull', 'push')); //Register the methods where we're looking for
+        $man->registerParameters(array('supported', 'comment')); //Register the parameters where we're looking for
+        $ret = new Action();
+        try {
+            $datas = $man->transform(
+                new JsonImporter(APP_DIR . '/../config/features.json'), //
+                new WawiExporter()
+            );
+            $ret->setResult($datas);
+            unset($man);
+            $ret->setHandled(true);
+        } catch (\Exception $e) {
+            if (isset($e->jtl)) {
+                $ret->setHandled($e->jtl);
+            }
+            else {
+                $ret->setHandled(false);
+            }
+            $err = new Error();
+            $err->setCode($e->getCode());
+            $err->setMessage($e->getMessage());
+            $ret->setError($err);
+        }
         return $ret;
     }
 
     /**
-     * Writes the controller configuration and returns the result.
+     * Write is not supported for features.
      * 
      * @param mixed $params An array of parameters to write
      */
     public function push($params = null)
     {
-        return $params;
+        unset($params); //We don't need the params parameter
+        $ret = new Action();
+        $ret->setHandled(false);
+        return $ret;
     }
 
     /**
-     * Delete is not needed.
+     * Delete is not supported for features.
      * 
      * @param mixed $params An array of parameters to write
      */
     public function delete($params = null)
     {
-        return $params;
+        unset($params); //We don't need the params parameter
+        $ret = new Action();
+        $ret->setHandled(false);
+        return $ret;
     }
 
 }
