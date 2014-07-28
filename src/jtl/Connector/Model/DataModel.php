7bd1408bad0164eb2e53da2a4fad2bbe9ae38a9e
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2014-07-28 11:59:55 +0200

    New base methods setProperty and validateType

--- src/jtl/Connector/Model/DataModel.php
+++ src/jtl/Connector/Model/DataModel.php
@@ -79,4 +79,34 @@ class DataModel extends CoreModel
 
         return $this;
     }
+
+    protected function setProperty($name, $value, $type)
+    {
+        if (!$this->validateType($value, $type)) {
+            throw new \InvalidArgumentException(sprintf("expected type %s, given value %s.", $type, gettype($value)));
+        }
+
+        $this->{$name} = $value;
+        
+        return $this;
+    }
+
+    protected function validateType($value, $type)
+    {
+        switch ($type)
+        {
+            case 'boolean':
+                return is_bool($value);
+            case 'integer':
+                return is_integer($value);
+            case 'float':
+                return is_float($value);
+            case 'string':
+                return is_string($value);
+            case 'array':
+                return is_array($value);
+            default:
+                throw new \InvalidArgumentException('type validator not found');
+        }
+    }
 }
