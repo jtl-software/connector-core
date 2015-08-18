9eff62502e5b149028f28a473d14509fec8e4711
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-08-18 12:23:42 +0200

    new entity shippingmethod

---
+++ src/jtl/Connector/Model/ShippingMethod.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @copyright 2010-2015 JTL-Software GmbH
+ * @package jtl\Connector\Model
+ * @subpackage Product
+ */
+
+namespace jtl\Connector\Model;
+
+use DateTime;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ *
+ * @access public
+ * @package jtl\Connector\Model
+ * @subpackage Product
+ * 
+ * @Serializer\AccessType("public_method")
+ */
+class ShippingMethod extends DataModel
+{
+    /**
+     * @var Identity 
+     * @Serializer\Type("jtl\Connector\Model\Identity")
+     * @Serializer\SerializedName("id")
+     * @Serializer\Accessor(getter="getId",setter="setId")
+     */
+    protected $id = null;
+
+    /**
+     * @var string 
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("name")
+     * @Serializer\Accessor(getter="getName",setter="setName")
+     */
+    protected $name = '';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->id = new Identity();
+    }
+
+    /**
+     * @param Identity $id 
+     * @return \jtl\Connector\Model\ShippingMethod
+     * @throws \InvalidArgumentException if the provided argument is not of type 'Identity'.
+     */
+    public function setId(Identity $id)
+    {
+        return $this->setProperty('id', $id, 'Identity');
+    }
+
+    /**
+     * @return Identity 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name 
+     * @return \jtl\Connector\Model\ShippingMethod
+     */
+    public function setName($name)
+    {
+        return $this->setProperty('name', $name, 'string');
+    }
+
+    /**
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
