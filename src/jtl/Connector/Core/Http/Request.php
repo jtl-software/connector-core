ca45964b1d818767cfe2afabf50ecff8d54b32fd
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-12-10 11:54:43 +0100

    Revert

--- src/jtl/Connector/Core/Http/Request.php
+++ src/jtl/Connector/Core/Http/Request.php
@@ -110,15 +110,11 @@ class Request
      */
     public static function stripData($data)
     {
-        /*
-        if (get_magic_quotes_gpc()) {
+        if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
             return stripslashes($data);
         }
         
         return $data;
-        */
-
-        return stripslashes($data);
     }
     
     /**
