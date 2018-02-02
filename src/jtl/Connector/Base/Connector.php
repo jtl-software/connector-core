1e7c0367fb9cf6eb76f932701793eb29be1072ac
Daniel Böhmer <boehmer.daniel@gmail.com>
2018-02-02 08:43:27 +0100

    Fixed base connector instantiation

--- src/jtl/Connector/Base/Connector.php
+++ src/jtl/Connector/Base/Connector.php
@@ -23,7 +23,7 @@ use \Symfony\Component\EventDispatcher\EventDispatcher;
  * @access public
  * @author Daniel Böhmer <daniel.boehmer@jtl-software.de>
  */
-abstract class Connector extends Singleton implements IEndpointConnector
+class Connector extends Singleton implements IEndpointConnector
 {
     protected $controller;
     protected $keyMapper;
