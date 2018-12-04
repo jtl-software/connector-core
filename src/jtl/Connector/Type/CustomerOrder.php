1961fef6651eae8b8e4653a00ee98f07a1e1eb01
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2018-12-04 12:53:03 +0100

    shippingMethodId added part 2

--- src/jtl/Connector/Type/CustomerOrder.php
+++ src/jtl/Connector/Type/CustomerOrder.php
@@ -35,6 +35,7 @@ class CustomerOrder extends DataType
             new PropertyInfo('shippingAddress', '\jtl\Connector\Model\CustomerOrderShippingAddress', null, false, false, true),
             new PropertyInfo('shippingDate', 'DateTime', null, false, false, false),
             new PropertyInfo('shippingInfo', 'string', '', false, false, false),
+            new PropertyInfo('shippingMethodId', 'Identity', null, false, true, false),
             new PropertyInfo('shippingMethodName', 'string', '', false, false, false),
             new PropertyInfo('status', 'string', '', false, false, false),
             new PropertyInfo('totalSum', 'double', 0.0, false, false, false),
