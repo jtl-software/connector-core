d2870d23484c6d2f306d8dbe704b1615c58ea6be
Benjamin Schaffrath <benjamin.schaffrath@jtl-software.com>
2019-10-21 14:22:15 +0200

    Refactoring for 7.1 Type Hints

--- src/Base/Connector.php
+++ src/Base/Connector.php
@@ -1,12 +1,13 @@
 <?php
 /**
- *
  * @copyright 2010-2013 JTL-Software GmbH
  * @package jtl\Connector\Base
  */
+
 namespace jtl\Connector\Base;
 
 use jtl\Connector\Authentication\ITokenValidator;
+use jtl\Connector\Core\Controller\IController;
 use \jtl\Connector\Core\Rpc\RequestPacket;
 use \jtl\Connector\Application\IEndpointConnector;
 use \jtl\Connector\Core\Utilities\Singleton;
@@ -15,6 +16,7 @@ use \jtl\Connector\Core\Rpc\Method;
 use \jtl\Connector\Mapper\IPrimaryKeyMapper;
 use \jtl\Connector\Authentication\ITokenLoader;
 use \jtl\Connector\Checksum\IChecksumLoader;
+use jtl\Connector\Result\Action;
 use \Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
@@ -25,125 +27,137 @@ use \Symfony\Component\EventDispatcher\EventDispatcher;
  */
 class Connector extends Singleton implements IEndpointConnector
 {
+    /** @var IController */
     protected $controller;
+    /** @var IPrimaryKeyMapper */
     protected $keyMapper;
+    /** @var ITokenLoader */
     protected $tokenLoader;
+    /** @var ITokenValidator */
     protected $tokenValidator;
+    /** @var IChecksumLoader */
     protected $checksumLoader;
+    /** @var EventDispatcher */
     protected $eventDispatcher;
+    /** @var Method */
     protected $method;
+    /** @var bool */
     protected $useSuperGlobals = true;
+    /** @var string */
     protected $modelNamespace = 'jtl\Connector\Model';
-
+    
     public function initialize()
     {
         
     }
-
+    
     /**
      * Setter primary key mapper
      *
      * @param \jtl\Connector\Mapper\IPrimaryKeyMapper $mapper
      * @return \jtl\Connector\Base\Connector
      */
-    public function setPrimaryKeyMapper(IPrimaryKeyMapper $mapper)
+    public function setPrimaryKeyMapper(IPrimaryKeyMapper $mapper): IEndpointConnector
     {
         $this->keyMapper = $mapper;
+        
         return $this;
     }
-
+    
     /**
      * Returns primary key mapper
      *
      * @return \jtl\Connector\Authentication\ITokenLoader
      */
-    public function getPrimaryKeyMapper()
+    public function getPrimaryKeyMapper(): IPrimaryKeyMapper
     {
         return $this->keyMapper;
     }
-
+    
     /**
      * Setter token loader
      *
      * @param \jtl\Connector\Authentication\ITokenLoader $tokenLoader
      * @return Connector
      */
-    public function setTokenLoader(ITokenLoader $tokenLoader)
+    public function setTokenLoader(ITokenLoader $tokenLoader): IEndpointConnector
     {
         $this->tokenLoader = $tokenLoader;
+        
         return $this;
     }
-
+    
     /**
      * Returns token loader
      *
      * @return \jtl\Connector\Authentication\ITokenLoader
      */
-    public function getTokenLoader()
+    public function getTokenLoader(): ITokenLoader
     {
         return $this->tokenLoader;
     }
-
+    
     /**
      * Setter token validator
      *
      * @param \jtl\Connector\Authentication\ITokenValidator $tokenValidator
      * @return Connector
      */
-    protected function setTokenValidator(ITokenValidator $tokenValidator)
+    protected function setTokenValidator(ITokenValidator $tokenValidator): IEndpointConnector
     {
         $this->tokenValidator = $tokenValidator;
+        
         return $this;
     }
-
+    
     /**
      * @return \jtl\Connector\Authentication\ITokenValidator
      */
-    public function getTokenValidator()
+    public function getTokenValidator(): ITokenValidator
     {
         return $this->tokenValidator;
     }
-
+    
     /**
      * Setter checksum loader
      *
      * @param \jtl\Connector\Checksum\IChecksumLoader $checksumLoader
      * @return Connector
      */
-    public function setChecksumLoader(IChecksumLoader $checksumLoader)
+    public function setChecksumLoader(IChecksumLoader $checksumLoader): IEndpointConnector
     {
         $this->checksumLoader = $checksumLoader;
+        
         return $this;
     }
-
+    
     /**
      * Returns checksum loader
      *
      * @return \jtl\Connector\Checksum\IChecksumLoader
      */
-    public function getChecksumLoader()
+    public function getChecksumLoader(): IChecksumLoader
     {
         return $this->checksumLoader;
     }
-
+    
     /**
      * Setter checksum loader
      *
      * @param Symfony\Component\EventDispatcher\EventDispatcher $eventDispatcher
      * @return Connector
      */
-    public function setEventDispatcher(EventDispatcher $eventDispatcher)
+    public function setEventDispatcher(EventDispatcher $eventDispatcher): IEndpointConnector
     {
         $this->eventDispatcher = $eventDispatcher;
+        
         return $this;
     }
-
+    
     /**
-     * Returns checksum loader
-     *
-     * @return Symfony\Component\EventDispatcher\EventDispatcher
+     * @return EventDispatcher
      */
-    public function getEventDispatcher()
+    public function getEventDispatcher(): EventDispatcher
     {
         return $this->eventDispatcher;
     }
@@ -154,9 +168,10 @@ class Connector extends Singleton implements IEndpointConnector
      * @param \jtl\Connector\Core\Rpc\Method $method
      * @return Connector
      */
-    public function setMethod(Method $method)
+    public function setMethod(Method $method): IEndpointConnector
     {
         $this->method = $method;
+        
         return $this;
     }
     
@@ -165,32 +180,34 @@ class Connector extends Singleton implements IEndpointConnector
      *
      * @return \jtl\Connector\Core\Rpc\Method
      */
-    public function getMethod()
+    public function getMethod(): Method
     {
         return $this->method;
     }
-
+    
     /**
      * @return boolean
      */
-    public function getUseSuperGlobals()
+    public function getUseSuperGlobals(): bool
     {
         return $this->useSuperGlobals;
     }
-
+    
     /**
      * Method Setter
      *
      * @param string $modelNamespace
      * @return Connector
      */
-    public function setModelNamespace($modelNamespace)
+    public function setModelNamespace(string $modelNamespace): IEndpointConnector
     {
         if (!is_string($modelNamespace) || $modelNamespace[strlen($modelNamespace) - 1] == '\\' || $modelNamespace[0] == '\\') {
-            throw new \InvalidArgumentException(sprintf('Wrong Namespace (%s) syntax. Example: jtl\Connector\Model', $modelNamespace));
+            throw new \InvalidArgumentException(sprintf('Wrong Namespace (%s) syntax. Example: jtl\Connector\Model',
+                $modelNamespace));
         }
-
+        
         $this->modelNamespace = $modelNamespace;
+        
         return $this;
     }
     
@@ -199,17 +216,16 @@ class Connector extends Singleton implements IEndpointConnector
      *
      * @return string
      */
-    public function getModelNamespace()
+    public function getModelNamespace(): string
     {
         return $this->modelNamespace;
     }
     
     /**
      * (non-PHPdoc)
-     *
      * @see \jtl\Connector\Application\IEndpointConnector::canHandle()
      */
-    public function canHandle()
+    public function canHandle(): bool
     {
         $controller = RpcMethod::buildController($this->getMethod()->getController());
         
@@ -217,7 +233,7 @@ class Connector extends Singleton implements IEndpointConnector
         if (class_exists($class)) {
             $this->controller = $class::getInstance();
             $this->action = $this->getMethod()->getAction();
-        
+            
             return method_exists($this->controller, $this->action);
         }
         
@@ -225,23 +241,21 @@ class Connector extends Singleton implements IEndpointConnector
     }
     
     /**
-     * (non-PHPdoc)
-     *
+     * @param RequestPacket $requestpacket
+     * @return Action
      * @see \jtl\Connector\Application\IEndpointConnector::handle()
      */
-    public function handle(RequestPacket $requestpacket)
+    public function handle(RequestPacket $requestpacket): Action
     {
         $this->controller->setMethod($this->getMethod());
         
         return $this->controller->{$this->action}($requestpacket->getParams());
     }
-
+    
     /**
-     * (non-PHPdoc)
-     *
      * @see \jtl\Connector\Application\IEndpointConnector::getController()
      */
-    public function getController()
+    public function getController(): IController
     {
         return $this->controller;
     }
