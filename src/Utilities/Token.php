2ca482eb15cebeec1375a407eca0b9ed311a2a8d
Patryk Gorol <patryk.gorol@jtl-software.com>
2019-12-19 11:23:02 +0100

    Token generation util
    Added source info

---
+++ src/Utilities/Token.php
@@ -0,0 +1,34 @@
+<?php
+namespace Jtl\Connector\Core\Utilities;
+
+/**
+ * Class Token
+ * @package Jtl\Connector\Core\Utilities
+ */
+class Token
+{
+    /**
+     * Generate UUIDv4
+     *
+     * @see https://github.com/Fleshgrinder/php-uuid/blob/php-7.1/src/UUID.php
+     * @return string
+     * @throws \Exception
+     */
+    public static function generate(): string
+    {
+        $bytes = random_bytes(16);
+
+        $bytes{6} = chr((ord($bytes{6}) & 0b00001111) | 0b01000000);
+        $bytes{8} = chr((ord($bytes{8}) & 0b00111111) | 0b10000000);
+
+        $hex = bin2hex($bytes);
+
+        $format = substr($hex, 0, 8) . '-' .
+            substr($hex, 8, 4) . '-' .
+            substr($hex, 12, 4) . '-' .
+            substr($hex, 16, 4) . '-' .
+            substr($hex, 20);
+
+        return strtoupper($format);
+    }
+}
---
+++ tests/Core/Utilities/TokenTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Jtl\Connector\Test\Core\Utilities;
+
+use Jtl\Connector\Core\Utilities\Token;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TokenTest
+ * @package Jtl\Connector\Test\Core\Utilities
+ */
+class TokenTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testGenerateToken()
+    {
+        $token = Token::generate();
+        $this->assertEquals(36, strlen($token));
+
+        $uuidV4Pattern = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/';
+
+        $match = preg_match($uuidV4Pattern, $token);
+
+        $this->assertEquals(1, $match);
+    }
+}
