03ecf018799a2cec4d7058f399f0d48911bb555b
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-03-17 17:12:33 +0100

    Added manufacturer factory

---
+++ src/Model/Generator/ManufacturerFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jtl\Connector\Core\Model\Generator;
+
+use Jtl\Connector\Core\Model\Manufacturer;
+
+/**
+ * Class ManufacturerFactory
+ * @package Jtl\Connector\Core\Model\Generator
+ */
+class ManufacturerFactory extends AbstractModelFactory
+{
+    protected $sort = 1;
+
+    /**
+     * @param array $override
+     * @return array
+     * @throws \Jawira\CaseConverter\CaseConverterException
+     */
+    public function makeOneArray(array $override = []): array
+    {
+        return array_merge([
+            'id' => $this->getFactory('Identity')->makeOneArray(),
+            'name' => $this->faker->text(40),
+            'sort' => $this->sort++,
+            'i18ns' => $this->getFactory('ManufacturerI18n')->makeArray(3)
+        ], $override);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return Manufacturer::class;
+    }
+}
---
+++ src/Model/Generator/ManufacturerI18nFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jtl\Connector\Core\Model\Generator;
+
+use Jtl\Connector\Core\Model\ManufacturerI18n;
+
+/**
+ * Class ManufacturerI18nFactory
+ * @package Jtl\Connector\Core\Model\Generator
+ */
+class ManufacturerI18nFactory extends AbstractModelFactory
+{
+    /**
+     * @param array $override
+     * @return array
+     */
+    public function makeOneArray(array $override = []): array
+    {
+        return array_merge([
+            'description' => $this->faker->text(),
+            'metaDescription' => $this->faker->text(),
+            'metaKeywords' => $this->faker->text(),
+            'titleTag' => $this->faker->text(),
+            'languageIso' => $this->faker->languageCode
+        ], $override);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return ManufacturerI18n::class;
+    }
+}
