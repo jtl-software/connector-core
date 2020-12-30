928be18a485f8f32ba28f12b3b0ca11b4e81cfb6
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-12-30 12:52:18 +0100

    Added translatable attribute factories

---
+++ src/Model/Generator/TranslatableAttributeFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jtl\Connector\Core\Model\Generator;
+
+use Jtl\Connector\Core\Model\TranslatableAttribute;
+
+/**
+ * Class TranslatableAttributeFactory
+ * @package Jtl\Connector\Core\Model\Generator
+ */
+class TranslatableAttributeFactory extends AbstractModelFactory
+{
+    /**
+     * @return array
+     */
+    protected function makeFakeArray(): array
+    {
+        return [
+            'isTranslated' => $this->faker->boolean,
+            'isCustomProperty' => $this->faker->boolean,
+            'i18ns' => $this->getFactory('TranslatableAttributeI18n')->makeArray(3)
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return TranslatableAttribute::class;
+    }
+}
---
+++ src/Model/Generator/TranslatableAttributeI18nFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jtl\Connector\Core\Model\Generator;
+
+use Jtl\Connector\Core\Model\TranslatableAttributeI18n;
+
+/**
+ * Class TranslatableAttributeI18nFactory
+ * @package Jtl\Connector\Core\Model\Generator
+ */
+class TranslatableAttributeI18nFactory extends AbstractModelFactory
+{
+    /**
+     * @return array
+     */
+    protected function makeFakeArray(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'value' => $this->faker->randomElement([
+                true,
+                false,
+                $this->faker->words(3, true)
+            ]),
+            'languageIso' => $this->faker->languageCode,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return TranslatableAttributeI18n::class;
+    }
+}
