1f398a85ccb8d9b01229bd14047bd6599f895d1c
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2021-02-05 10:06:23 +0100

    Removed outdated properties and methods

--- src/Model/AbstractDataModel.php
+++ src/Model/AbstractDataModel.php
@@ -9,12 +9,10 @@ namespace Jtl\Connector\Core\Model;
 
 use DateTime;
 use InvalidArgumentException;
-use Jtl\Connector\Core\Exception\NotImplementedException;
 use JMS\Serializer\Annotation as Serializer;
 use Jtl\Connector\Core\Type\AbstractDataType;
 use ReflectionClass;
 use ReflectionException;
-use stdClass;
 
 /**
  * Entity data model
@@ -33,55 +31,6 @@ abstract class AbstractDataModel extends AbstractModel
      * @Serializer\Exclude
      */
     private $modelType = null;
-
-    /**
-     * @var boolean
-     * @Serializer\Type("boolean")
-     * @Serializer\AccessType("reflection")
-     * @Serializer\Exclude
-     */
-    protected $isEncrypted = false;
-
-    /**
-     * @var array list of strings
-     * @Serializer\Type("array<string, string>")
-     * @Serializer\Exclude
-     * @Serializer\ReadOnly
-     */
-    protected $fields = [];
-
-    /**
-     * Fields Getter
-     *
-     * @return mixed:string
-     */
-    public function getFields(): array
-    {
-        return $this->fields;
-    }
-
-    /**
-     * Get a Model Member Name
-     *
-     * @param boolean $toWawi
-     * @param string $key
-     * @return mixed:string|NULL
-     */
-    public function getField(bool $toWawi = false, string $key): ?string
-    {
-        if ($this->fields !== null && is_array($this->fields)) {
-            $fields = $this->fields;
-            if (!$toWawi) {
-                $fields = array_flip($fields);
-            }
-
-            if (is_array($fields) && isset($fields[$key])) {
-                return $fields[$key];
-            }
-        }
-
-        return null;
-    }
 
     /**
      * @return AbstractDataType
@@ -105,129 +54,5 @@ abstract class AbstractDataModel extends AbstractModel
         }
 
         return $this->modelType;
-    }
-
-    /**
-     * Encrypted Status
-     *
-     * @return boolean
-     */
-    public function isEncrypted(): bool
-    {
-        return $this->isEncrypted;
-    }
-
-    /**
-     * @param string $propertyName
-     * @param string|null $endpoint
-     * @param int|null $host
-     * @return AbstractDataModel
-     * @throws ReflectionException
-     */
-    public function setIdentity(string $propertyName, string $endpoint = null, int $host = null): AbstractDataModel
-    {
-        foreach ($this->getModelType()->getProperties() as $propertyInfo) {
-            $property = ucfirst($propertyInfo->getName());
-            $getter = 'get' . $property;
-
-            if ($propertyInfo->isNavigation() && !is_null($this->{$getter}())) {
-                if (is_array($this->{$getter}())) {
-                    $list = $this->{$getter}();
-                    foreach ($list as &$entity) {
-                        if ($entity instanceof self) {
-                            $entity->setIdentity($propertyName, $endpoint, $host);
-                        } elseif ($entity instanceof Identity && $propertyName === $propertyInfo->getName()) {
-                            if (!is_null($endpoint)) {
-                                $entity->setEndpoint($endpoint);
-                            }
-
-                            if (!is_null($host)) {
-                                $entity->setHost($host);
-                            }
-                        }
-                    }
-                } elseif ($this->{$getter}() instanceof self) {
-                    /* @var self $entity */
-                    $entity = $this->{$getter}();
-                    $entity->setIdentity($propertyName, $endpoint, $host);
-                }
-            } elseif ($propertyInfo->isIdentity() && $propertyName === $propertyInfo->getName()) {
-                /* @var Identity $identity */
-                $identity = $this->{$getter}();
-
-                if (!is_null($endpoint)) {
-                    $identity->setEndpoint($endpoint);
-                }
-
-                if (!is_null($host)) {
-                    $identity->setHost($host);
-                }
-            }
-        }
-
-        return $this;
-    }
-
-    protected function setProperty($name, $value, $type): AbstractDataModel
-    {
-        if (!$this->validateType($value, $type)) {
-            throw new InvalidArgumentException(sprintf(
-                "%s (%s): expected type '%s', given value '%s'.",
-                $name,
-                get_class($this),
-                $type,
-                gettype($value)
-            ));
-        }
-
-        $this->{$name} = $value;
-
-        return $this;
-    }
-
-    protected function validateType($value, $type): bool
-    {
-        if ($value === null) {
-            return true;
-        }
-
-        switch ($type) {
-            case 'boolean':
-            case 'bool':
-                return is_bool($value);
-            case 'integer':
-            case 'int':
-                return is_integer($value);
-            case 'float':
-            case 'double':
-                return (is_float($value) || is_integer($value) || is_double($value));
-            case 'string':
-                return is_string($value);
-            case 'array':
-                return is_array($value);
-            case 'Identity':
-                return ($value instanceof Identity);
-            case 'DateTime':
-                return ($value instanceof DateTime);
-            default:
-                if (is_object($value)) {
-                    return is_null($value) || is_subclass_of($value, 'Jtl\Connector\Core\Model\AbstractDataModel');
-                }
-
-                throw new InvalidArgumentException(sprintf("type '%s' validator not found", $type));
-        }
-    }
-
-
-    /**
-     * Object Mapping
-     *
-     * @param boolean $toWawi
-     * @param mixed $obj Object to map
-     * @throws NotImplementedException
-     */
-    public function map(bool $toWawi = false, \stdClass $obj = null)
-    {
-        throw new NotImplementedException;
     }
 }
