f200d6a29acbd04cf510398a0156a316641e950e
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 15:39:02 +0200

    codestyle LoggerException

--- src/Exception/LoggerException.php
+++ src/Exception/LoggerException.php
@@ -4,8 +4,7 @@ namespace Jtl\Connector\Core\Exception;
 
 class LoggerException extends \Exception
 {
-    public const
-        FORMATTER_NOT_EXISTS = 10;
+    public const FORMATTER_NOT_EXISTS = 10;
 
     /**
      * @param string $className
@@ -13,6 +12,6 @@ class LoggerException extends \Exception
      */
     public static function formatterNotExists(string $className): self
     {
-        return new self(sprintf('Formatter class "%s" does not exist', $className), self::FORMATTER_NOT_EXISTS);
+        return new self(\sprintf('Formatter class "%s" does not exist', $className), self::FORMATTER_NOT_EXISTS);
     }
 }
