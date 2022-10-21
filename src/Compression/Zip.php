f2d136065b6819eb942dc6c76a180af445a95f45
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 10:34:04 +0200

    codestyle Zip

--- src/Compression/Zip.php
+++ src/Compression/Zip.php
@@ -27,16 +27,18 @@ class Zip
      */
     public function extract(string $sourceFile, string $targetFolder): bool
     {
-        if (!class_exists('ZipArchive')) {
+        if (!\class_exists('ZipArchive')) {
             throw new CompressionException('Class ZipArchive not found. PHP 5 >= 5.2.0, PECL zip >= 1.1.0 installed?');
         }
 
-        if (!file_exists($sourceFile)) {
-            throw new FileNotFoundException(sprintf('File (%s) not found', $sourceFile));
+        if (!\file_exists($sourceFile)) {
+            throw new FileNotFoundException(\sprintf('File (%s) not found', $sourceFile));
         }
 
-        if (!is_dir($targetFolder) || !is_writeable($targetFolder)) {
-            throw new \InvalidArgumentException(sprintf('Folder (%s) does not exists or is not writeable', $targetFolder));
+        if (!\is_dir($targetFolder) || !\is_writable($targetFolder)) {
+            throw new \InvalidArgumentException(
+                \sprintf('Folder (%s) does not exists or is not writeable', $targetFolder)
+            );
         }
 
         $archive = new \ZipArchive();
