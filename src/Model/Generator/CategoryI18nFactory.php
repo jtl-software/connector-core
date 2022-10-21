e25789832b0d9b98a465145bd9f397ec99ebfb6e
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 16:02:37 +0200

    codestyle CategoryI18nFactory

--- src/Model/Generator/CategoryI18nFactory.php
+++ src/Model/Generator/CategoryI18nFactory.php
@@ -12,17 +12,18 @@ class CategoryI18nFactory extends AbstractModelFactory
 {
     /**
      * @return array
+     * @throws \Exception
      */
     protected function makeFakeArray() : array
     {
         return [
-            'description' => $this->faker->sentence,
+            'description'     => $this->faker->sentence,
             'metaDescription' => $this->faker->text,
-            'metaKeywords' => $this->faker->words(mt_rand(0, 5), true),
-            'name' => $this->faker->word,
-            'titleTag' => $this->faker->word,
-            'urlPath' => $this->faker->url,
-            'languageIso' => $this->faker->languageCode
+            'metaKeywords'    => $this->faker->words(\random_int(0, 5), true),
+            'name'            => $this->faker->word,
+            'titleTag'        => $this->faker->word,
+            'urlPath'         => $this->faker->url,
+            'languageIso'     => $this->faker->languageCode
         ];
     }
 
