98504448c8f0d76c95f9cdc7c191acebd12907fa
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 15:12:36 +0200

    codestyle ErrorCode

--- src/Definition/ErrorCode.php
+++ src/Definition/ErrorCode.php
@@ -8,15 +8,15 @@ namespace Jtl\Connector\Core\Definition;
 final class ErrorCode
 {
     public const
-        NO_SESSION = 789,
-        AUTHENTICATION_FAILED = 790,
-        INVALID_SESSION = -32000,
-        UNINITIALIZED_SESSION = -32001,
-        INVALID_REQUEST = -32600,
-        PARSE_ERROR = -32700,
-        INVALID_METHOD = -32701,
-        UNKNOWN_CONTROLLER = 33000,
-        UNKNOWN_ACTION = 33001,
-        REQUEST_ERROR = 400,
-        SERVER_ERROR = 500;
+    NO_SESSION            = 789,
+    AUTHENTICATION_FAILED = 790,
+    INVALID_SESSION       = -32000,
+    UNINITIALIZED_SESSION = -32001,
+    INVALID_REQUEST       = -32600,
+    PARSE_ERROR           = -32700,
+    INVALID_METHOD        = -32701,
+    UNKNOWN_CONTROLLER    = 33000,
+    UNKNOWN_ACTION        = 33001,
+    REQUEST_ERROR         = 400,
+    SERVER_ERROR          = 500;
 }
