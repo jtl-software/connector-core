30b1d82d3f8757951f5564bd209bb18919f80054
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 15:14:05 +0200

    codestyle IdentityType

--- src/Definition/IdentityType.php
+++ src/Definition/IdentityType.php
@@ -5,57 +5,57 @@ namespace Jtl\Connector\Core\Definition;
 class IdentityType
 {
     public const
-        CATEGORY = 1,
-        CUSTOMER = 2,
-        CUSTOMER_ORDER = 4,
-        DELIVERY_NOTE = 8,
-        MANUFACTURER = 32,
-        PRODUCT = 64,
-        SPECIFIC = 128,
-        SPECIFIC_VALUE = 256,
-        PAYMENT = 512,
-        CROSS_SELLING = 1024,
-        CROSS_SELLING_GROUP = 2048,
-        CROSS_SELLING_ITEM = 72,
-        SHIPPING_CLASS = 4096,
-        CONFIG_GROUP = 6,
-        CONFIG_ITEM = 10,
-        CURRENCY = 12,
-        CUSTOMER_GROUP = 14,
-        LANGUAGE = 18,
-        UNIT = 20,
-        MEASUREMENT_UNIT = 22,
-        PRODUCT_TYPE = 24,
-        SHIPPING_METHOD = 26,
-        TAX_CLASS = 70,
-        TAX_RATE = 28,
-        WAREHOUSE = 30,
-        CATEGORY_ATTRIBUTE = 34,
-        PRODUCT_ATTRIBUTE = 36,
-        PRODUCT_VARIATION = 38,
-        PRODUCT_VARIATION_VALUE = 40,
-        PRODUCT_IMAGE = 42,
-        CATEGORY_IMAGE = 44,
-        MANUFACTURER_IMAGE = 46,
-        PRODUCT_VARIATION_VALUE_IMAGE = 48,
-        SPECIFIC_IMAGE = 50,
-        SPECIFIC_VALUE_IMAGE = 52,
-        CONFIG_GROUP_IMAGE = 54,
-        PRODUCT_WAREHOUSE_INFO = 56,
-        PRODUCT_STOCK_LEVEL = 58,
-        PRODUCT_TO_CATEGORY = 60,
-        PRODUCT_PRICE = 62,
-        PRODUCT_SPECIFIC = 66,
-        CUSTOMER_ORDER_ITEM = 68;
+    CATEGORY                      = 1,
+    CUSTOMER                      = 2,
+    CUSTOMER_ORDER                = 4,
+    DELIVERY_NOTE                 = 8,
+    MANUFACTURER                  = 32,
+    PRODUCT                       = 64,
+    SPECIFIC                      = 128,
+    SPECIFIC_VALUE                = 256,
+    PAYMENT                       = 512,
+    CROSS_SELLING                 = 1024,
+    CROSS_SELLING_GROUP           = 2048,
+    CROSS_SELLING_ITEM            = 72,
+    SHIPPING_CLASS                = 4096,
+    CONFIG_GROUP                  = 6,
+    CONFIG_ITEM                   = 10,
+    CURRENCY                      = 12,
+    CUSTOMER_GROUP                = 14,
+    LANGUAGE                      = 18,
+    UNIT                          = 20,
+    MEASUREMENT_UNIT              = 22,
+    PRODUCT_TYPE                  = 24,
+    SHIPPING_METHOD               = 26,
+    TAX_CLASS                     = 70,
+    TAX_RATE                      = 28,
+    WAREHOUSE                     = 30,
+    CATEGORY_ATTRIBUTE            = 34,
+    PRODUCT_ATTRIBUTE             = 36,
+    PRODUCT_VARIATION             = 38,
+    PRODUCT_VARIATION_VALUE       = 40,
+    PRODUCT_IMAGE                 = 42,
+    CATEGORY_IMAGE                = 44,
+    MANUFACTURER_IMAGE            = 46,
+    PRODUCT_VARIATION_VALUE_IMAGE = 48,
+    SPECIFIC_IMAGE                = 50,
+    SPECIFIC_VALUE_IMAGE          = 52,
+    CONFIG_GROUP_IMAGE            = 54,
+    PRODUCT_WAREHOUSE_INFO        = 56,
+    PRODUCT_STOCK_LEVEL           = 58,
+    PRODUCT_TO_CATEGORY           = 60,
+    PRODUCT_PRICE                 = 62,
+    PRODUCT_SPECIFIC              = 66,
+    CUSTOMER_ORDER_ITEM           = 68;
 
-    protected static $types = null;
+    protected static ?array $types = null;
 
     /**
      * @return integer[]
      */
     public static function getTypes(): array
     {
-        if (is_null(self::$types)) {
+        if (\is_null(self::$types)) {
             self::$types = (new \ReflectionClass(self::class))->getConstants();
         }
 
@@ -68,6 +68,6 @@ class IdentityType
      */
     public static function isType(int $type): bool
     {
-        return in_array($type, self::getTypes(), true);
+        return \in_array($type, self::getTypes(), true);
     }
 }
