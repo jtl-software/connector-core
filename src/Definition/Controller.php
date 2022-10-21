90d45054f4543961017ced4441efdc534ebe400b
Julian Langer <julian.langer@jtl-software.com>
2022-10-21 15:11:45 +0200

    codestyle Controller

--- src/Definition/Controller.php
+++ src/Definition/Controller.php
@@ -5,21 +5,21 @@ namespace Jtl\Connector\Core\Definition;
 final class Controller
 {
     public const
-        CATEGORY = 'Category',
-        CONNECTOR = 'Connector',
-        CROSSSELLING = 'CrossSelling',
-        CUSTOMER = 'Customer',
-        CUSTOMER_ORDER = 'CustomerOrder',
-        DELIVERY_NOTE = 'DeliveryNote',
-        GLOBAL_DATA = 'GlobalData',
-        IMAGE = 'Image',
-        MANUFACTURER = 'Manufacturer',
-        PAYMENT = 'Payment',
-        PRODUCT = 'Product',
-        PRODUCT_PRICE = 'ProductPrice',
-        PRODUCT_STOCK_LEVEL = 'ProductStockLevel',
-        SPECIFIC = 'Specific',
-        STATUS_CHANGE = 'StatusChange';
+    CATEGORY            = 'Category',
+    CONNECTOR           = 'Connector',
+    CROSSSELLING        = 'CrossSelling',
+    CUSTOMER            = 'Customer',
+    CUSTOMER_ORDER      = 'CustomerOrder',
+    DELIVERY_NOTE       = 'DeliveryNote',
+    GLOBAL_DATA         = 'GlobalData',
+    IMAGE               = 'Image',
+    MANUFACTURER        = 'Manufacturer',
+    PAYMENT             = 'Payment',
+    PRODUCT             = 'Product',
+    PRODUCT_PRICE       = 'ProductPrice',
+    PRODUCT_STOCK_LEVEL = 'ProductStockLevel',
+    SPECIFIC            = 'Specific',
+    STATUS_CHANGE       = 'StatusChange';
 
     /**
      * @var string[]|null
@@ -28,11 +28,10 @@ final class Controller
 
     /**
      * @return integer[]
-     * @throws \ReflectionException
      */
     public static function getControllers(): array
     {
-        if (is_null(self::$controllers)) {
+        if (\is_null(self::$controllers)) {
             self::$controllers = (new \ReflectionClass(self::class))->getConstants();
         }
 
@@ -40,12 +39,12 @@ final class Controller
     }
 
     /**
-     * @param integer $controllerName
+     * @param string $controllerName
+     *
      * @return boolean
-     * @throws \ReflectionException
      */
     public static function isController(string $controllerName): bool
     {
-        return in_array($controllerName, self::getControllers(), true);
+        return \in_array($controllerName, self::getControllers(), true);
     }
 }
