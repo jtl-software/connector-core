8d35c3bcba47564e25b92caeca9918d412ca8002
Tim Platzke <tim.platzke@jtl-software.com>
2023-05-03 16:45:35 +0200

    add fingerscrossed handler add more processors

--- src/Logger/LoggerService.php
+++ src/Logger/LoggerService.php
@@ -5,11 +5,18 @@ namespace Jtl\Connector\Core\Logger;
 use Jtl\Connector\Core\Exception\LoggerException;
 use Monolog\Formatter\FormatterInterface;
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Handler\FingersCrossedHandler;
+use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\RotatingFileHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use Monolog\Logger as MonoLogger;
+use Monolog\Processor\HostnameProcessor;
 use Monolog\Processor\IntrospectionProcessor;
+use Monolog\Processor\MemoryPeakUsageProcessor;
 use Monolog\Processor\ProcessorInterface;
 use Monolog\Processor\PsrLogMessageProcessor;
+use Monolog\Processor\WebProcessor;
 
 class LoggerService
 {
@@ -44,7 +51,7 @@ class LoggerService
     /**
      * @var integer
      */
-    protected $maxFiles = 7;
+    protected $maxFiles = 2;
 
     /**
      * @var ProcessorInterface
@@ -52,19 +59,66 @@ class LoggerService
     protected $processors = [];
 
     /**
+     * Final handler that gets wrapped by FingersCrossedHandler
+     * @var HandlerInterface
+     */
+    protected $handler;
+
+    /**
+     * FingersCrossedHandler for combined log file
+     * @var HandlerInterface
+     */
+    protected $combinedHandler;
+
+    /**
      * LoggerFactory constructor.
      * @param string $logDir
      * @param string $logLevel
      * @param int $maxFiles
      */
-    public function __construct(string $logDir, string $logLevel, int $maxFiles = 7)
+    public function __construct(string $logDir, string $logLevel, int $maxFiles = 2)
     {
         $this->logDir   = $logDir;
         $this->logLevel = $logLevel;
         $this->maxFiles = $maxFiles;
         $this
+            // should work with FingerCrossedHandler, might add useless data
             ->pushProcessor(new IntrospectionProcessor())
-            ->pushProcessor(new PsrLogMessageProcessor());
+            ->pushProcessor(new PsrLogMessageProcessor())
+            ->pushProcessor(new MemoryPeakUsageProcessor())
+            ->pushProcessor(new WebProcessor(
+                [
+                    // strip jtlauth value from uri
+                    // string replace is faster as regex
+                    'REQUEST_URI'     => \str_replace(
+                        \sprintf('jtlauth=%s', $_GET['jtlauth'] ?? ''),
+                        'jtlauth=***',
+                        $_SERVER['REQUEST_URI'] ?? ''
+                    ),
+                    // might be useful
+                    'REQUEST_METHOD'  => $_SERVER['REQUEST_METHOD'],
+                    'HTTP_USER_AGENT' => $_SERVER['HTTP_USER_AGENT'],
+                    // useful for SaaS Connectors
+                    'REQUEST_DOMAIN'  => $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'] ?? '',
+                    // post request size in MB
+                    'REQUEST_SIZE'    => \round(
+                        \strlen(\file_get_contents('php://input')) / 1024 / 1024,
+                        4
+                    ) . ' MB',
+                ],
+                [
+                    'url'           => 'REQUEST_URI',
+                    'http_method'   => 'REQUEST_METHOD',
+                    'user_agent'    => 'HTTP_USER_AGENT',
+                    'domain'        => 'REQUEST_DOMAIN',
+                    'request_size'  => 'REQUEST_SIZE',
+                ]
+            ))
+            ->pushProcessor(new HostnameProcessor());
+
+        $fileName = \sprintf('%s/combined.log', $this->logDir);
+        $this->combinedHandler  = new RotatingFileHandler($fileName, $this->maxFiles, Logger::DEBUG);
+        $this->createHandler();
     }
 
     /**
@@ -74,6 +128,29 @@ class LoggerService
     public function setLogLevel(string $logLevel): void
     {
         $this->logLevel = $logLevel;
+        $this->createHandler();
+        // need to close all channels to update the handler
+        foreach ($this->channels as $channel) {
+            $channel->close();
+        }
+        $this->channels = [];
+    }
+
+    /**
+     * (re-)created the handler for the combined log file
+     * @return void
+     */
+    protected function createHandler(): void
+    {
+        // needed if we change the passthru level
+        if($this->handler instanceof FingersCrossedHandler) {
+            $this->handler->close();
+        }
+        $handler = new FingersCrossedHandler($this->combinedHandler, Logger::ERROR, 0, true, true, $this->logLevel);
+        if(!\is_null($this->formatter)) {
+            $handler->setFormatter($this->formatter);
+        }
+        $this->handler = $handler;
     }
 
     /**
@@ -83,6 +160,22 @@ class LoggerService
     public function has(string $channel): bool
     {
         return isset($this->channels[\lcfirst($channel)]);
+    }
+
+    /**
+     * creates legacy handler for each channel
+     * @param string $channel
+     * @param Level|int $logLevel
+     * @return HandlerInterface
+     */
+    protected function createChannelSpecificHandler(string $channel, Level|int $logLevel): HandlerInterface
+    {
+        $fileName = \sprintf('%s/%s.log', $this->logDir, $channel);
+        $handler  = new RotatingFileHandler($fileName, $this->maxFiles, $logLevel);
+        if (!\is_null($this->formatter)) {
+            $handler->setFormatter($this->formatter);
+        }
+        return $handler;
     }
 
     /**
@@ -98,12 +191,9 @@ class LoggerService
 
         $logLevel = MonoLogger::toMonologLevel($this->logLevel);
         if (!$this->channels[$channel]->isHandling($logLevel)) {
-            $fileName = \sprintf('%s/%s.log', $this->logDir, $channel);
-            $handler  = new RotatingFileHandler($fileName, $this->maxFiles, $logLevel);
-            if (!\is_null($this->formatter)) {
-                $handler->setFormatter($this->formatter);
-            }
+            $handler = $this->createChannelSpecificHandler($channel, $logLevel);
             $this->channels[$channel]->pushHandler($handler);
+            $this->channels[$channel]->pushHandler($this->handler);
             foreach ($this->processors as $processor) {
                 $this->channels[$channel]->pushProcessor($processor);
             }
