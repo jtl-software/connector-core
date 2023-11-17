bb26d712438a3b0a1b75e8eabcd1d14a8c186a9b
Tim Platzke <tim.platzke@jtl-software.com>
2023-11-17 09:48:58 +0100

    add chunked handler to logger service.

---
+++ src/Logger/Handler/ChunkedHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jtl\Connector\Core\Logger\Handler;
+
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Handler\FormattableHandlerInterface;
+use Monolog\Handler\Handler;
+use Monolog\Handler\HandlerInterface;
+use Monolog\LogRecord;
+
+class ChunkedHandler extends Handler
+{
+
+    private HandlerInterface $nextHandler;
+
+    private int $chunkSize = self::MAX_LOG_ENTRY_LENGTH;
+
+
+    public const
+        MAX_LOG_ENTRY_LENGTH = 31320;
+
+    public function __construct(HandlerInterface $nextHandler, ?int $chunkSize = null)
+    {
+        $this->nextHandler = $nextHandler;
+        if ($chunkSize !== null) {
+            $this->chunkSize = $chunkSize;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isHandling(LogRecord $record): bool
+    {
+        return $this->nextHandler->isHandling($record);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(LogRecord $record): bool
+    {
+        // false means continue to bubble
+        $return  = false;
+        $message = $record->message;
+        if ($this->chunkSize > 0 && \strlen($message) > $this->chunkSize) {
+            $chunks   = \str_split($message, $this->chunkSize);
+            $total    = \count($chunks);
+            $recordId = \md5($message);
+            foreach ($chunks as $key => $chunk) {
+                $newRecord                    = clone $record;
+                $newRecord->message           = \sprintf("(part %i/%i) %s", $key, $total, $chunk);
+                $newRecord->extra['recordId'] = $recordId;
+
+                $return = $this->nextHandler->handle($newRecord) ?? $return;
+            }
+            return $return;
+        }
+        return $this->nextHandler->handle($record);
+    }
+
+    public function setFormatter(FormatterInterface $formatter): HandlerInterface
+    {
+        if (
+            $this->nextHandler instanceof FormattableHandlerInterface
+            || \method_exists($this->nextHandler, 'setFormatter')
+        ) {
+            return $this->nextHandler->setFormatter($formatter);
+        }
+        throw new \UnexpectedValueException(
+            'The nested handler of type ' . \get_class($this->nextHandler) . ' does not support formatters.'
+        );
+    }
+
+    public function getFormatter(): FormatterInterface
+    {
+        if (
+            $this->nextHandler instanceof FormattableHandlerInterface
+            || \method_exists($this->nextHandler, 'getFormatter')
+        ) {
+            return $this->nextHandler->getFormatter();
+        }
+        throw new \UnexpectedValueException(
+            'The nested handler of type ' . \get_class($this->nextHandler) . ' does not support formatters.'
+        );
+    }
+}
