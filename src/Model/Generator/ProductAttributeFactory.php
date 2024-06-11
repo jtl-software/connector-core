9ce83708534187656a41306f0e699a14963a4374
Julian Langer <julian.langer@jtl-software.com>
2024-06-11 15:22:18 +0200

    add factory

---
+++ src/Model/Generator/ProductAttributeFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Jtl\Connector\Core\Model\Generator;
+
+use Jtl\Connector\Core\Model\ProductAttribute;
+
+class ProductAttributeFactory extends TranslatableAttributeFactory
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return ProductAttribute::class;
+    }
+}
