d9cedc35e9d448895074f59a807ff6fd8c343f8c
Julian Langer <julian.langer@jtl-software.com>
2024-06-18 16:11:13 +0200

    fix null return type

--- src/Config/CoreConfigInterface.php
+++ src/Config/CoreConfigInterface.php
@@ -13,39 +13,39 @@ interface CoreConfigInterface extends ConfigInterface
      * @param string    $valueName
      * @param bool|null $default
      *
-     * @return bool
+     * @return bool|null
      * @throws \TypeError
      * @throws ConfigException
      */
-    public function getBool(string $valueName, ?bool $default = null): bool;
+    public function getBool(string $valueName, ?bool $default = null): ?bool;
 
     /**
      * @param string      $valueName
      * @param string|null $default
      *
-     * @return string
+     * @return string|null
      * @throws ConfigException
      * @throws \TypeError
      */
-    public function getString(string $valueName, ?string $default = null): string;
+    public function getString(string $valueName, ?string $default = null): ?string;
 
     /**
      * @param string   $valueName
      * @param int|null $default
      *
-     * @return int
+     * @return int|null
      * @throws ConfigException
      * @throws \TypeError
      */
-    public function getInt(string $valueName, ?int $default = null): int;
+    public function getInt(string $valueName, ?int $default = null): ?int;
 
     /**
      * @param string     $valueName
      * @param float|null $default
      *
-     * @return float
+     * @return float|null
      * @throws ConfigException
      * @throws \TypeError
      */
-    public function getFloat(string $valueName, ?float $default = null): float;
+    public function getFloat(string $valueName, ?float $default = null): ?float;
 }
