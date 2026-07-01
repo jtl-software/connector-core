85f8adf5d2fc0a282cb3572aa4f20d945d3c2e58
akempe <andreas.kempe@jtl-software.com>
2026-07-01 16:01:48 +0200

    change php min requ to php8.3, fix language bug in LanguageIsoSubscriber for empty language value

--- src/Serializer/Subscriber/LanguageIsoSubscriber.php
+++ src/Serializer/Subscriber/LanguageIsoSubscriber.php
@@ -71,7 +71,7 @@ class LanguageIsoSubscriber implements EventSubscriberInterface
     public function onPreDeserialize(PreDeserializeEvent $event): void
     {
         $data = $event->getData();
-        if (\is_array($data) && isset($data['languageISO']) && !isset($data['languageIso'])) {
+        if (\is_array($data) && empty($data['languageISO']) && !isset($data['languageIso'])) {
             $language            = $this->languages->fromISO_639_2b($data['languageISO']);
             $data['languageIso'] = $language->toISO_639_1();
             $event->setData($data);
