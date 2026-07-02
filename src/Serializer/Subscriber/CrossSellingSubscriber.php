9b908d35c1404aef1b4b009aebb6ad5a7a32d8f6
akempe <andreas.kempe@jtl-software.com>
2026-07-02 13:45:55 +0200

    readd guards for array checks

--- src/Serializer/Subscriber/CrossSellingSubscriber.php
+++ src/Serializer/Subscriber/CrossSellingSubscriber.php
@@ -36,7 +36,7 @@ class CrossSellingSubscriber implements EventSubscriberInterface
             /** @var array{productId?: array{0: string, 1: int}, items?: array<int, array<string, mixed>>} $data */
             $data      = $event->getData();
             $productId = $data['productId'][1] ?? 0;
-            if (isset($data['items'])) {
+            if (isset($data['items']) && \is_array($data['items'])) {
                 foreach ($data['items'] as $i => $item) {
                     if (!isset($data['items'][$i]['id'])) {
                         /** @var array{crossSellingGroupId?: array{0: string, 1: int}} $item */
