jtl-software/connector-core

commit b0ea7556e695dfe15502d9f4b3ac5fa28aef4aca

feat!: upgrade all dependencies to current stable versions
BREAKING CHANGE: Minimum PHP version raised from 7.4 to 8.2.
Multiple major dependency upgrades require consumers to align.

Dependencies upgraded:
- PHP >=7.4 -> >=8.2
- symfony/* ^5.0|^6.0 -> ^8.0 (console, event-dispatcher, filesystem, finder, http-foundation)
- doctrine/dbal ^3.0 -> ^4.0
- doctrine/collections ^2.2 -> ^3.0
- monolog/monolog ^2.0|^3.0 -> ^3.0
- php-di/php-di ^6.0 -> ^7.0
- phpunit/phpunit ^9.0 -> ^11.0
- phpstan/phpstan ^1.0 -> ^2.0
- squizlabs/php_codesniffer ^3.10 -> ^4.0
- slevomat/coding-standard ^8.15 -> ^8.29
- micheh/phpcs-gitlab ^1.1 -> ^2.0
- jtl/connector-cq * -> ^2.0
- mockery/mockery ^1.3 -> ^1.6

Dependencies removed:
- doctrine/annotations (zero usage)
- doctrine/cache (zero usage)
- jtl/unit-test (helpers inlined into tests/src/dbc/TestCase.php)

DBAL 4 migration:
- Rewrite QueryBuilder to track query type internally (INSERT/UPDATE/DELETE/SELECT)
- Replace execute() with executeQuery()/executeStatement()
- Replace fetch()/fetchAll() with fetchAssociative()/fetchAllAssociative()
- Replace getSchemaManager() with createSchemaManager()
- Replace Schema::getMigrateToSql() with Comparator + Platform::getAlterSchemaSQL()
- Replace Type::getName() with TypeRegistry::lookupName()
- Replace Connection::PARAM_STR_ARRAY with ArrayParameterType::STRING
- Replace DBALException with Exception, SchemaException with ColumnDoesNotExist::new()
- Replace ConversionException::conversionFailedInvalidType() with InvalidType::new()
- Add normalizeIntrospectedSchema() for SQLite type round-trip mismatches
- Update platform class references (MySqlPlatform -> MySQLPlatform, SqlitePlatform -> SQLitePlatform)
- Update connection params (driver key mandatory, pdo key removed)

Symfony 8 migration:
- Replace removed Request::get() with Request::request->get() / Request::query->get()
- Replace Request mock with real Request in tests (typed  property)

Monolog 3 / PHP-DI 7:

src/Application/Application.php
@@ -10,8 +10,6 @@ use DI\ContainerBuilder;
 use DI\Definition\Exception\InvalidDefinition;
 use DI\DependencyException;
 use DI\NotFoundException;
-use Doctrine\Common\Annotations\AnnotationException;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Jawira\CaseConverter\CaseConverterException;
 use JMS\Serializer\Exception\LogicException;
 use JMS\Serializer\Exception\NotAcceptableException;
@@ -113,8 +111,8 @@ use Throwable;
 
 class Application
 {
-    public const PROTOCOL_VERSION = 7;
-    public const MIN_PHP_VERSION  = '7.4';
+    public const int    PROTOCOL_VERSION = 7;
+    public const string MIN_PHP_VERSION  = '8.2';
     /** @var array<string, string> */
     protected static array    $mimeTypeToExtensionMappings = [
         'image/bmp'                => 'bmp',
@@ -165,18 +163,17 @@ class Application
      * @throws ApplicationException
      * @throws ConfigException
      * @throws DependencyException
+     * @throws EmptyDirectoryException
      * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
+     * @throws InvalidDefinition
+     * @throws \JMS\Serializer\Exception\InvalidArgumentException
+     * @throws \JMS\Serializer\Exception\RuntimeException
      * @throws LoggerException
+     * @throws LogicException
+     * @throws \LogicException
      * @throws ReflectionException
      * @throws RuntimeException
-     * @throws InvalidDefinition
-     * @throws AnnotationException
-     * @throws \InvalidArgumentException
-     * @throws \JMS\Serializer\Exception\InvalidArgumentException
-     * @throws LogicException
-     * @throws \JMS\Serializer\Exception\RuntimeException
-     * @throws \LogicException
-     * @throws EmptyDirectoryException
      * @throws \TypeError
      * @throws \UnexpectedValueException
      */
@@ -188,8 +185,6 @@ class Application
         if (!\is_dir($connectorDir)) {
             throw ApplicationException::connectorDirNotExists($connectorDir);
         }
-        AnnotationRegistry::registerLoader('class_exists');
-
         if ($configSchema !== null && $config !== null) {
             if ($config instanceof ConfigSchemaConfigInterface) {
                 $config->setConfigSchema($configSchema);
@@ -204,8 +199,7 @@ class Application
 
         $serializerCacheDir = null;
         if (
-            $config instanceof CoreConfigInterface
-            && $config->getBool(ConfigSchema::DEBUG, false) === false
+            $config->getBool(ConfigSchema::DEBUG, false) === false
             && $config->getBool(ConfigSchema::SERIALIZER_ENABLE_CACHE, true) === true
         ) {
             $serializerCacheDir = $config->getString(ConfigSchema::CACHE_DIR);
@@ -215,7 +209,6 @@ class Application
         $this->config       = $config;
         $this->configSchema = $configSchema;
         $this->container    = (new ContainerBuilder())
-            ->useAnnotations(true)
             ->useAutowiring(true)
             ->build();
 
@@ -310,14 +303,14 @@ class Application
      * @throws DependencyException
      * @throws FileNotFoundException
      * @throws NotFoundException
+     * @throws \ReflectionException
      * @throws RpcException
      * @throws SessionException
      * @throws Throwable
-     * @throws \ReflectionException
      */
     public function run(ConnectorInterface $connector): void
     {
-        $jtlrpc = Validate::string($this->httpRequest->get('jtlrpc', ''));
+        $jtlrpc = Validate::string($this->httpRequest->request->get('jtlrpc', ''));
         $this->httpResponse->setLogger($this->loggerService->get(LoggerService::CHANNEL_RPC));
         $this->eventDispatcher->addSubscriber(new RequestParamsTransformSubscriber());
         $this->eventDispatcher->addSubscriber(new FeaturesSubscriber());
@@ -413,17 +406,17 @@ class Application
      *
      *
      * @return void
+     * @throws BadRequestException
      * @throws DatabaseException
      * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
      * @throws RuntimeException
      * @throws SessionException
-     * @throws \InvalidArgumentException
-     * @throws BadRequestException
      * @throws \UnexpectedValueException
      */
     protected function startSession(string $rpcMethod): void
     {
-        $sessionId   = $this->httpRequest->get('jtlauth');
+        $sessionId   = $this->httpRequest->query->get('jtlauth');
         $sessionName = 'JtlConnector';
 
         if ($sessionId === null && $rpcMethod !== RpcMethod::AUTH) {
@@ -454,9 +447,9 @@ class Application
      * @return SessionHandlerInterface
      * @throws DatabaseException
      * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
      * @throws RuntimeException
      * @throws SessionException
-     * @throws \InvalidArgumentException
      * @throws \UnexpectedValueException
      */
     public function getSessionHandler(): SessionHandlerInterface
@@ -506,9 +499,7 @@ class Application
             $sqlite->connect(['location' => $dbLocation]);
 
             $collector = new SqliteSyncErrorCollector($sqlite);
-            if ($collector instanceof LoggerAwareInterface) {
-                $collector->setLogger($this->loggerService->get(LoggerService::CHANNEL_GLOBAL));
-            }
+            $collector->setLogger($this->loggerService->get(LoggerService::CHANNEL_GLOBAL));
 
             $this->container->set(SyncErrorCollectorInterface::class, $collector);
 
@@ -527,10 +518,10 @@ class Application
      * @return void
      * @throws DatabaseException
      * @throws InvalidArgumentException
-     * @throws RuntimeException
-     * @throws SessionException
      * @throws \InvalidArgumentException
      * @throws \LogicException
+     * @throws RuntimeException
+     * @throws SessionException
      * @throws \UnexpectedValueException
      */
     protected function prepareContainer(ConnectorInterface $connector): void
@@ -623,13 +614,13 @@ class Application
      * @throws DefinitionException
      * @throws DependencyException
      * @throws FileNotFoundException
+     * @throws \InvalidArgumentException
      * @throws LinkerException
      * @throws NotFoundException
      * @throws ReflectionException
      * @throws RpcException
      * @throws RuntimeException
      * @throws Throwable
-     * @throws \InvalidArgumentException
      */
     protected function execute(
         ConnectorInterface $connector,
@@ -722,6 +713,9 @@ class Application
                 case Action::CLEAR:
                 case Action::FINISH:
                 case Action::INIT:
+                    if (!\is_bool($result)) {
+                        throw new \RuntimeException('$result must be a bool.');
+                    }
                     $eventArg = new BoolEvent($result);
                     break;
                 case Action::IDENTIFY:
@@ -756,13 +750,13 @@ class Application
      * @throws DefinitionException
      * @throws DependencyException
      * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
+     * @throws \JMS\Serializer\Exception\RuntimeException
      * @throws LinkerException
      * @throws LogicException
+     * @throws NotAcceptableException
      * @throws NotFoundException
      * @throws ReflectionException
-     * @throws \InvalidArgumentException
-     * @throws NotAcceptableException
-     * @throws \JMS\Serializer\Exception\RuntimeException
      * @throws UnsupportedFormatException
      */
     protected function createHandleRequest(
@@ -814,10 +808,16 @@ class Application
             $eventArg = null;
             switch ($action) {
                 case Action::ACK:
+                    if (!$param instanceof Ack) {
+                        throw new \RuntimeException('$param must be instance of Ack.');
+                    }
                     $eventArg = new AckEvent($param);
                     break;
                 case Action::PUSH:
                 case Action::DELETE:
+                    if (!$param instanceof AbstractModel) {
+                        throw new \RuntimeException('$param must be instance of AbstractModel.');
+                    }
                     /** @var IdentityLinker $identityLinker */
                     $identityLinker = $this->container->get(IdentityLinker::class);
                     $identityLinker->linkModel($param);
@@ -828,10 +828,19 @@ class Application
                     break;
                 case Action::PULL:
                 case Action::STATISTIC:
+                    if (!$param instanceof QueryFilter) {
+                        throw new \RuntimeException('$param must be instance of QueryFilter.');
+                    }
                     $eventArg = new QueryFilterEvent($param);
                     break;
                 case Action::CLEAR:
+                    if (!$param instanceof Identities) {
+                        throw new \RuntimeException('$param must be instance of Identities.');
+                    }
                     foreach ($param->getIdentities() as $relationType => $identities) {
+                        if ($identities === null) {
+                            continue;
+                        }
                         foreach ($identities as $identity) {
                             /** @var IdentityLinker $identityLinker */
                             $identityLinker = $this->container->get(IdentityLinker::class);
@@ -879,8 +888,8 @@ class Application
      * @throws ApplicationException
      * @throws CompressionException
      * @throws DefinitionException
-     * @throws FileNotFoundException
      * @throws \Exception
+     * @throws FileNotFoundException
      */
     protected function handleImagePush(AbstractImage ...$images): void
     {
@@ -1012,6 +1021,7 @@ class Application
         }
 
         $controller = $this->container->get($controllerName);
+        \assert(\is_object($controller));
         if ($controller instanceof LoggerAwareInterface) {
             /** @var LoggerInterface $loggerInterface */
             $loggerInterface = $this->container->get(LoggerInterface::class);
@@ -1036,17 +1046,17 @@ class Application
                 }
 
                 try {
+                    \assert(\method_exists($controller, $action));
+                    /** @var AbstractModel[] $dataModels */
                     $dataModels = $controller->$action(...$params);
 
                     foreach ($dataModels as $dataModel) {
-                        if ($dataModel instanceof AbstractModel) {
-                            /** @var IdentityLinker $identityLinker */
-                            $identityLinker = $this->container->get(IdentityLinker::class);
-                            $identityLinker->linkModel($dataModel, ($request->getAction() === Action::DELETE));
-                            /** @var ChecksumLinker $checksumLinker */
-                            $checksumLinker = $this->container->get(ChecksumLinker::class);
-                            $checksumLinker->link($dataModel);
-                        }
+                        /** @var IdentityLinker $identityLinker */
+                        $identityLinker = $this->container->get(IdentityLinker::class);
+                        $identityLinker->linkModel($dataModel, ($request->getAction() === Action::DELETE));
+                        /** @var ChecksumLinker $checksumLinker */
+                        $checksumLinker = $this->container->get(ChecksumLinker::class);
+                        $checksumLinker->link($dataModel);
                         $result[] = $dataModel;
                     }
 
@@ -1088,18 +1098,23 @@ class Application
                 }
                 break;
             case Action::IDENTIFY:
+                \assert(\method_exists($controller, $action));
+                /** @var ConnectorIdentification $result */
                 $result = $controller->$action($connector);
                 break;
             default:
+                \assert(\method_exists($controller, $action));
                 $param  = \count($params) > 0 ? \reset($params) : null;
                 $result = $controller->$action($param);
                 break;
         }
 
         if ($action === Action::STATISTIC && $controller instanceof StatisticInterface) {
-            $result = (new Statistic())
+            /** @var int $statisticCount */
+            $statisticCount = $result;
+            $result         = (new Statistic())
                 ->setControllerName($controllerName)
-                ->setAvailable((int)$result);
+                ->setAvailable($statisticCount);
         }
 
         if (!$result instanceof Response) {

commit 7f11f1d289484e8824fbc868a2f7c2231c040c18

fix: restore session token and DI injection behavior lost in pre-6 upgrade (CO-3594)
The dependency upgrade in b0ea7556 introduced two regressions in Application:

1. startSession() read jtlauth via `$this->httpRequest->query->get()`, i.e.
   the URL query string only. Clients that transmit the session token in the
   POST body (e.g. the connector web tester) then hit SessionException
   "No session" on every non-auth request even though AUTH returns a valid
   sessionId. Restore the query+body read `$this->httpRequest->get('jtlauth')`.

2. The DI ContainerBuilder dropped `->useAnnotations(true)` (PHP-DI 6) with no
   replacement, so under PHP-DI 7 constructor injection configured via
   attributes/annotations no longer works — scalar constructor arguments like
   a controller's `bool $debug` become unresolvable ("no value defined or
   guessable"). Add the PHP-DI 7 equivalent `->useAttributes(true)` so
   `#[Inject]` attributes are honoured again.

Both restore behavior every connector relied on before the upgrade.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

src/Application/Application.php
@@ -210,6 +210,7 @@ class Application
         $this->configSchema = $configSchema;
         $this->container    = (new ContainerBuilder())
             ->useAutowiring(true)
+            ->useAttributes(true)
             ->build();
 
         $logLevel = Validate::string($this->config->get(ConfigSchema::LOG_LEVEL));
@@ -416,7 +417,7 @@ class Application
      */
     protected function startSession(string $rpcMethod): void
     {
-        $sessionId   = $this->httpRequest->query->get('jtlauth');
+        $sessionId   = $this->httpRequest->get('jtlauth');
         $sessionName = 'JtlConnector';
 
         if ($sessionId === null && $rpcMethod !== RpcMethod::AUTH) {